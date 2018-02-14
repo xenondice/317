82a55d376b052c6c32ea1b1e82e4702b42827b4d
Mariusler <mariuler@stud.ntnu.no>
2018-02-14 12:15:29 +0100

    Optimaliserte arduino kode, trenger testing

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -9,19 +9,38 @@
 #define DATA_PIN 6
 
 // INITIATORS FOR SERIAL COMMUNICATION
-String inputString = "";         // a String to hold incoming data
-boolean stringComplete = false;  // whether the string is complete
+uint8_t buffer[NUM_LEDS*3];
+//String inputString = "";         // a String to hold incoming data
+int numBytesRead = 0;   // How many bytes have we read into the buffer
+bool gotData = false;
 
-// INITIATORS FOR LEDS AND LED UPDATE MODE
+// INITIATORS FOR LEDS
 CRGB leds[NUM_LEDS];
 
-// Function for reading data from serial and setting corresponding LEDS
 // Each led has 3 bytes of data (One for each color value)
-void setLedsFromInputString(String input){
-  uint8_t value;
-  int ledCount = 0;
+void setLedsFromBuffer(){
   int colorCount = 0;
-  int lastSpaceIndex = 0;
+  int ledCount = 0;
+  for(int i = 0; i< NUM_LEDS*3; i++){
+    switch(colorCount){
+        case 0:
+          leds[ledCount].r = buffer[i];
+          break;
+        case 1:
+          leds[ledCount].g = buffer[i];
+          break;
+        case 2:
+          leds[ledCount].b = buffer[i];
+          break;
+      }
+      colorCount++;
+      if(colorCount == 3){
+        colorCount = 0;
+        ledCount++;
+      }
+  }
+
+  /*
   for(int i = 0; i <= input.length(); i++){
     if(input[i] == ' '){
       currentSpaceIndex = i;
@@ -45,24 +64,27 @@ void setLedsFromInputString(String input){
       }
     }
   }
+  */
 }
 
 // SETUP AND MAIN LOOP
 void setup(){
-  Serial.begin(9600);              //Starting serial communication
-  inputString.reserve(NUM_LEDS*3); //Buffer for incoming data
-  // randomSeed(analogRead(0));
+  Serial.begin(57600);              //Starting serial communication
+  //inputString.reserve(NUM_LEDS*3); //Buffer for incoming data
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  leds[0].r = 255;
+  leds[1].g = 255;
+  leds[2].b = 255;
+  FastLED.show();
 }
 
 void loop(){
   // Handle incoming serial data from PC, Pioneer LX or Raspberry Pi
-  if(stringComplete){
+  if(gotData){
     //Serial.println(inputString);
-    setLedsFromInputString(inputString);
-    inputString = "";
-    stringComplete = false;
-    FastLed.show();
+    setLedsFromBuffer();
+    gotData = false;
+    FastLED.show();
   }
   delay(1);
 }
@@ -77,11 +99,12 @@ void loop(){
   https://www.arduino.cc/en/Tutorial/SerialEvent
 */
 void serialEvent(){
-  while(Serial.available()){
-    char inChar = (char)Serial.read();
-    inputString += inChar;
-    if (inChar == '\n') {
-      stringComplete = true;
+  while(Serial.available() && !gotData){
+    buffer[numBytesRead] = Serial.read();
+    //inputString += inChar;
+    numBytesRead++;
+    if(numBytesRead == NUM_LEDS*3){
+      gotData = true;
     }
   }
 }
