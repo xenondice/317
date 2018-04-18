aba82d58cabcec707388baa242a53beebb2242c6
Mariusler <mariuler@stud.ntnu.no>
2018-04-18 11:49:05 +0200

    Fixed some typos

--- neural_interpeters/frequency.py
+++ neural_interpeters/frequency.py
@@ -6,13 +6,13 @@ class frequency_plot:
     def __init__(self):
         self.high = 0
         self.low = 0
+        self.intensities = [0]*60
         self.initialized = False
 
     def update(self, input_data, output_data):
-        intensities = [0]*60
-        median = statistics.median(sorted(updatedata))
-        max_frequency = max(updatedata)
-        min_frequency = min(updatedata)
+        median = statistics.median(sorted(input_data))
+        max_frequency = max(input_data)
+        min_frequency = min(input_data)
         if max_frequency >= 4* median:   ## Done to avoid very dominant nodes maybe implement better solution
             max_frequency = 2*median    #TODO better?
         if self.initialized == False:
@@ -22,9 +22,9 @@ class frequency_plot:
         else:
             self.high = self.high*0.9 + 0.1*max_frequency
             self.low = self.low*0.9 + 0.1*min_frequency
-        for index in range(len(updatedata)):
-            intens = int((updatedata[index]-self.low)/(self.high - self.low)*10)
-            intens = min(9, intens)
-            intens = max(0, intens)
-            intensities[index] = intens
-        d2c.data_to_bytearray(intensities, output_data)
+        for index in range(len(input_data)):
+            intensity = int((input_data[index]-self.low)/(self.high - self.low)*10)
+            intensity = min(9, intensity)
+            intensity = max(0, intensity)
+            self.intensities[index] = intensity
+        d2c.data_to_bytearray(self.intensities, output_data)
