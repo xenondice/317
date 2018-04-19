24fe281d1a3c8ce603fa02a88f5a7a37ca56f597
andejoha <anders.johannessen@lyse.net>
2018-04-19 08:46:48 +0200

    TwoDPlot working :scream:

--- neural_sources/file/server_simulator.py
+++ neural_sources/file/server_simulator.py
@@ -2,19 +2,15 @@ import pandas as pd
 import system.settings as settings
 import time
 
-def read_CSV(fileName, N_ROWS, skip_rows=6):
-    return pd.read_csv(fileName, skiprows=skip_rows, nrows=N_ROWS, index_col=0)
+class FileServer:
+    def __init__(self):
+        pass
 
 
-def live_stream_skipes():
-    file_path = 'data/2017-10-20_MEA2_100000rows_10sec.csv'
-    n_lines = 10000/settings.LED_REFRESHES_PER_SECOND
-    skip = 6
+    def read_CSV(fileName, N_ROWS, skip_rows=6):
+        return pd.read_csv(fileName, skiprows=skip_rows, nrows=N_ROWS, index_col=0)
 
-    while skip < 100000:
-        raw_data = read_CSV(file_path, n_lines, skip_rows=skip)
-        skip += n_lines
-        time.sleep(1/settings.LED_REFRESHES_PER_SECOND)
+
 
 
 def live_stream_volt():
