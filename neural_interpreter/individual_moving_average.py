small fix in induvidual moving average

neural_interpreter/individual_moving_average.py
@@ -1,4 +1,4 @@
-import neural_interpeters.support_functions.data_to_color as d2c
+import neural_interpreter.support_functions.data_to_color as d2c
 import numpy as np
 from collections import deque
 
@@ -18,9 +18,11 @@ class IndividualMovingAverage:
         else:
             for i in range(len(input_data)):
                 self.history[i].append(input_data[i])
-        for i in range(len(input_data))
+        for i in range(len(input_data)):
             average = np.average(self.history[i])
-            std = np.std(self.hisory[i])
+            std = np.std(self.history[i])
+            if std==0:
+                std = 1
             intensity = int((input_data[i] - (average - 2 * std) / (4 * std) * 10))
             intensity = max(0, intensity)
             intensity = min(9, intensity)