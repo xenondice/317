ff00b66bb80eab0a77016e383ad41433832e29ca
Tormod Myrvang <tormod.myrvang@gmail.com>
2018-04-19 14:37:16 +0200

    moved moving average over to standard deviation

--- neural_interpeters/moving_average.py
+++ neural_interpeters/moving_average.py
@@ -1,27 +1,34 @@
 import statistics
 import neural_interpeters.support_functions.data_to_color as d2c
+from collections import deque
+import system.settings as settings
 
 # Takes in array of size 60 with frequencies
 class MovingAverage:
     def __init__(self):
-        self.high = 0
-        self.low = 0
-        self.intensities = [0]*60
-        self.initialized = False
+        self.history_length = 40
+        self.past_variances = deque([0] * self.history_length)
+        self.past_averages = deque([0] * self.history_length)
+        self.intensities = [0] * settings.NEURAL_ELECTRODES_TOTAL
 
     def render(self, input_data, output_data):
-        median = statistics.median(sorted(input_data))
-        max_frequency = max(input_data)
-        min_frequency = min(input_data)
-        if not self.initialized:
-            self.high = max_frequency
-            self.low = min_frequency
-            self.initialized = True
-        else:
-            self.high = self.high*0.9 + 0.1*max_frequency
-            self.low = self.low*0.9 + 0.1*min_frequency
+        current_variation = statistics.stdev(input_data)
+        current_average = statistics.mean(input_data)
+        self.past_variances.popleft()
+        self.past_variances.append(current_variation)
+        self.past_averages.popleft()
+        self.past_averages.append(current_average)
+
+        variation = statistics.mean(self.past_variances)
+        average = statistics.mean(self.past_averages)
+
+        low = average - variation
+        high = average + variation
+        if low == high:
+            high += 1
+            
         for index in range(len(input_data)):
-            intensity = int((input_data[index]-self.low)/(self.high - self.low)*10)
+            intensity = int((input_data[index]-low)/(high - low)*10)
             intensity = min(9, intensity)
             intensity = max(0, intensity)
             self.intensities[index] = intensity
